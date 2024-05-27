b58c695481c757f659bac6653429a33231302b7b
Phxsha <phashateboho@gmail.com>
2024-05-27 23:24:07 +0200

    new route stats added

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,3 +8,17 @@ from api.v1.views import app_views
 def get_status():
     """Returns the status of the API"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def get_stats():
+    """Retrieves the number of each objects by type"""
+    counts = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(counts)
