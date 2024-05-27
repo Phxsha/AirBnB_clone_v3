6e102d71804f9335be3be3dc95372f0b51631c93
Phxsha <phashateboho@gmail.com>
2024-05-27 16:17:27 +0200

    length

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -85,12 +85,14 @@ class TestBaseModel(unittest.TestCase):
         tic = datetime.utcnow()
         inst1 = BaseModel()
         toc = datetime.utcnow()
-        self.assertTrue(tic <= inst1.created_at <= toc, f"{tic} !<= {inst1.created_at} !<= {toc}")
+        self.assertTrue(tic <= inst1.created_at <= toc,f"{
+            tic} !<= {inst1.created_at} !<= {toc}")
         time.sleep(1e-4)
         tic = datetime.utcnow()
         inst2 = BaseModel()
         toc = datetime.utcnow()
-        self.assertTrue(tic <= inst2.created_at <= toc, f"{tic} !<= {inst2.created_at} !<= {toc}")
+        self.assertTrue(tic <= inst2.created_at <= toc, f"{
+            tic} !<= {inst2.created_at} !<= {toc}")
         self.assertEqual(inst1.created_at, inst1.updated_at)
         self.assertEqual(inst2.created_at, inst2.updated_at)
         self.assertNotEqual(inst1.created_at, inst2.created_at)
