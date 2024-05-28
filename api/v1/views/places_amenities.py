e0634b94d484c8c02fe8b9a8ae5a527e4fa3fa1f
root <phashateboho@gmail.com>
2024-05-28 13:09:04 -0700

    check pycode

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -8,7 +8,8 @@ from models.amenity import Amenity
 from os import getenv
 
 
-@app_views.route('/places/<place_id>/amenities', methods=['GET'], strict_slashes=False)
+@app_views.route('/places/<place_id>/amenities', methods=[
+    'GET'], strict_slashes=False)
 def get_amenities_of_place(place_id):
     """Retrieves the list of all Amenity objects of a Place"""
     place = storage.get(Place, place_id)
@@ -18,12 +19,14 @@ def get_amenities_of_place(place_id):
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         amenities = place.amenities
     else:
-        amenities = [storage.get(Amenity, amenity_id) for amenity_id in place.amenity_ids]
+        amenities = [storage.get(
+            Amenity, amenity_id) for amenity_id in place.amenity_ids]
 
     return jsonify([amenity.to_dict() for amenity in amenities])
 
 
-@app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=[
+    'DELETE'], strict_slashes=False)
 def delete_amenity_from_place(place_id, amenity_id):
     """Deletes a Amenity object from a Place"""
     place = storage.get(Place, place_id)
@@ -47,7 +50,8 @@ def delete_amenity_from_place(place_id, amenity_id):
     return jsonify({}), 200
 
 
-@app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['POST'], strict_slashes=False)
+@app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=[
+    'POST'], strict_slashes=False)
 def link_amenity_to_place(place_id, amenity_id):
     """Links a Amenity object to a Place"""
     place = storage.get(Place, place_id)
